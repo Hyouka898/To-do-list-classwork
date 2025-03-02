836bd0ddba122dee69c1d5243561dfa02a1fcb6d
lyhour1997/New-project.git <chlyhour168@gmail.com>
2025-03-02 12:59:19 +0700

    Layout-to-do-list

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Management</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    
+    <!-- Link to Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
     <style>
         .drawer {
             position: fixed;
@@ -80,9 +84,11 @@
                         </td>
                         <td><span class="badge bg-danger">Inactive</span></td>
                         <td>
-                            <button id="btnUpdate" class="btn btn-warning btn-sm">Update</button>
-                            <button id="btnViewDel" class="btn btn-info btn-sm text-white">View Detail</button>
-                            <button id="btnDelete" class="btn btn-danger btn-sm">Delete</button>
+                            <button id="btnUpdate" class="btn btn-warning btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>
+                            <button id="btnViewDel" class="btn btn-info btn-sm text-white">
+                                <i class="fa-solid fa-calendar-week"></i>
+                            </button>
+                            <button id="btnDelete" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
                         </td>
                     </tr>
 
